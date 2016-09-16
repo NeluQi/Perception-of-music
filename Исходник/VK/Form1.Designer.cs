341f966db5875f048c993370177c5262f95f3791
Egor <egor2998067@gmail.com>
2016-09-16 10:56:42 +0300

    Big Update
    Big Update

--- Исходник/VK/Form1.Designer.cs
+++ Исходник/VK/Form1.Designer.cs
@@ -30,22 +30,34 @@
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
             this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
-            this.button3 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel3 = new MetroFramework.Controls.MetroLabel();
+            this.metroTabControl1 = new MetroFramework.Controls.MetroTabControl();
+            this.metroTabPage1 = new MetroFramework.Controls.MetroTabPage();
+            this.metroTabPage2 = new MetroFramework.Controls.MetroTabPage();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.metroLabel6 = new MetroFramework.Controls.MetroLabel();
+            this.GroupComboBox1 = new MetroFramework.Controls.MetroComboBox();
+            this.metroLabel5 = new MetroFramework.Controls.MetroLabel();
+            this.CountTextBox = new MetroFramework.Controls.MetroTextBox();
+            this.IDTextBox = new MetroFramework.Controls.MetroTextBox();
+            this.metroLabel4 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.LoadCastomButton1 = new MetroFramework.Controls.MetroButton();
+            this.metroTabPage3 = new MetroFramework.Controls.MetroTabPage();
+            this.metroTextBox1 = new MetroFramework.Controls.MetroTextBox();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.metroButton2 = new MetroFramework.Controls.MetroButton();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.metroTabControl1.SuspendLayout();
+            this.metroTabPage1.SuspendLayout();
+            this.metroTabPage2.SuspendLayout();
+            this.metroTabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // axWindowsMediaPlayer1
-            // 
-            resources.ApplyResources(this.axWindowsMediaPlayer1, "axWindowsMediaPlayer1");
-            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
-            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
             // 
             // notifyIcon1
             // 
@@ -58,58 +70,221 @@
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.TabStop = false;
             // 
-            // button1
-            // 
-            resources.ApplyResources(this.button1, "button1");
-            this.button1.Name = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            resources.ApplyResources(this.button2, "button2");
-            this.button2.Name = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
             // listBox1
             // 
-            this.listBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.listBox1.BackColor = System.Drawing.Color.White;
             this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             resources.ApplyResources(this.listBox1, "listBox1");
             this.listBox1.FormattingEnabled = true;
             this.listBox1.Name = "listBox1";
+            this.listBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseClick);
             // 
-            // button3
+            // metroLabel2
             // 
-            resources.ApplyResources(this.button3, "button3");
-            this.button3.Name = "button3";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            resources.ApplyResources(this.metroLabel2, "metroLabel2");
+            this.metroLabel2.FontSize = MetroFramework.MetroLabelSize.Small;
+            this.metroLabel2.FontWeight = MetroFramework.MetroLabelWeight.Regular;
+            this.metroLabel2.Name = "metroLabel2";
+            // 
+            // metroLabel3
+            // 
+            resources.ApplyResources(this.metroLabel3, "metroLabel3");
+            this.metroLabel3.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.metroLabel3.Name = "metroLabel3";
+            // 
+            // metroTabControl1
+            // 
+            resources.ApplyResources(this.metroTabControl1, "metroTabControl1");
+            this.metroTabControl1.Controls.Add(this.metroTabPage1);
+            this.metroTabControl1.Controls.Add(this.metroTabPage2);
+            this.metroTabControl1.Controls.Add(this.metroTabPage3);
+            this.metroTabControl1.FontSize = MetroFramework.MetroTabControlSize.Tall;
+            this.metroTabControl1.Multiline = true;
+            this.metroTabControl1.Name = "metroTabControl1";
+            this.metroTabControl1.SelectedIndex = 0;
+            this.metroTabControl1.Style = MetroFramework.MetroColorStyle.Lime;
+            this.metroTabControl1.UseSelectable = true;
+            // 
+            // metroTabPage1
+            // 
+            this.metroTabPage1.Controls.Add(this.metroLabel2);
+            this.metroTabPage1.Controls.Add(this.metroLabel3);
+            this.metroTabPage1.Controls.Add(this.listBox1);
+            resources.ApplyResources(this.metroTabPage1, "metroTabPage1");
+            this.metroTabPage1.HorizontalScrollbarBarColor = true;
+            this.metroTabPage1.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroTabPage1.HorizontalScrollbarSize = 10;
+            this.metroTabPage1.Name = "metroTabPage1";
+            this.metroTabPage1.VerticalScrollbarBarColor = true;
+            this.metroTabPage1.VerticalScrollbarHighlightOnWheel = false;
+            this.metroTabPage1.VerticalScrollbarSize = 10;
+            // 
+            // metroTabPage2
+            // 
+            this.metroTabPage2.Controls.Add(this.metroButton1);
+            this.metroTabPage2.Controls.Add(this.metroLabel6);
+            this.metroTabPage2.Controls.Add(this.GroupComboBox1);
+            this.metroTabPage2.Controls.Add(this.metroLabel5);
+            this.metroTabPage2.Controls.Add(this.CountTextBox);
+            this.metroTabPage2.Controls.Add(this.IDTextBox);
+            this.metroTabPage2.Controls.Add(this.metroLabel4);
+            this.metroTabPage2.Controls.Add(this.metroLabel1);
+            this.metroTabPage2.Controls.Add(this.LoadCastomButton1);
+            this.metroTabPage2.HorizontalScrollbarBarColor = true;
+            this.metroTabPage2.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroTabPage2.HorizontalScrollbarSize = 10;
+            resources.ApplyResources(this.metroTabPage2, "metroTabPage2");
+            this.metroTabPage2.Name = "metroTabPage2";
+            this.metroTabPage2.VerticalScrollbarBarColor = true;
+            this.metroTabPage2.VerticalScrollbarHighlightOnWheel = false;
+            this.metroTabPage2.VerticalScrollbarSize = 10;
+            // 
+            // metroButton1
+            // 
+            resources.ApplyResources(this.metroButton1, "metroButton1");
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.UseSelectable = true;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // metroLabel6
+            // 
+            resources.ApplyResources(this.metroLabel6, "metroLabel6");
+            this.metroLabel6.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.metroLabel6.Name = "metroLabel6";
+            // 
+            // GroupComboBox1
+            // 
+            this.GroupComboBox1.FormattingEnabled = true;
+            resources.ApplyResources(this.GroupComboBox1, "GroupComboBox1");
+            this.GroupComboBox1.Items.AddRange(new object[] {
+            resources.GetString("GroupComboBox1.Items"),
+            resources.GetString("GroupComboBox1.Items1")});
+            this.GroupComboBox1.Name = "GroupComboBox1";
+            this.GroupComboBox1.UseSelectable = true;
+            // 
+            // metroLabel5
+            // 
+            resources.ApplyResources(this.metroLabel5, "metroLabel5");
+            this.metroLabel5.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.metroLabel5.Name = "metroLabel5";
+            // 
+            // CountTextBox
+            // 
+            this.CountTextBox.Lines = new string[0];
+            resources.ApplyResources(this.CountTextBox, "CountTextBox");
+            this.CountTextBox.MaxLength = 32767;
+            this.CountTextBox.Name = "CountTextBox";
+            this.CountTextBox.PasswordChar = '\0';
+            this.CountTextBox.PromptText = "Не более 100";
+            this.CountTextBox.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.CountTextBox.SelectedText = "";
+            this.CountTextBox.UseSelectable = true;
+            // 
+            // IDTextBox
+            // 
+            this.IDTextBox.Lines = new string[0];
+            resources.ApplyResources(this.IDTextBox, "IDTextBox");
+            this.IDTextBox.MaxLength = 32767;
+            this.IDTextBox.Name = "IDTextBox";
+            this.IDTextBox.PasswordChar = '\0';
+            this.IDTextBox.PromptText = "ID группы";
+            this.IDTextBox.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.IDTextBox.SelectedText = "";
+            this.IDTextBox.UseSelectable = true;
+            // 
+            // metroLabel4
+            // 
+            resources.ApplyResources(this.metroLabel4, "metroLabel4");
+            this.metroLabel4.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.metroLabel4.Name = "metroLabel4";
+            // 
+            // metroLabel1
+            // 
+            resources.ApplyResources(this.metroLabel1, "metroLabel1");
+            this.metroLabel1.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.metroLabel1.Name = "metroLabel1";
+            // 
+            // LoadCastomButton1
+            // 
+            resources.ApplyResources(this.LoadCastomButton1, "LoadCastomButton1");
+            this.LoadCastomButton1.Name = "LoadCastomButton1";
+            this.LoadCastomButton1.UseSelectable = true;
+            this.LoadCastomButton1.Click += new System.EventHandler(this.LoadCastomButton1_Click);
+            // 
+            // metroTabPage3
+            // 
+            this.metroTabPage3.Controls.Add(this.metroTextBox1);
+            this.metroTabPage3.HorizontalScrollbarBarColor = true;
+            this.metroTabPage3.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroTabPage3.HorizontalScrollbarSize = 10;
+            resources.ApplyResources(this.metroTabPage3, "metroTabPage3");
+            this.metroTabPage3.Name = "metroTabPage3";
+            this.metroTabPage3.VerticalScrollbarBarColor = true;
+            this.metroTabPage3.VerticalScrollbarHighlightOnWheel = false;
+            this.metroTabPage3.VerticalScrollbarSize = 10;
+            // 
+            // metroTextBox1
+            // 
+            this.metroTextBox1.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::VK.Properties.Settings.Default, "def", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.metroTextBox1.Lines = new string[] {
+        "Автор vk.com/id208497682",
+        "",
+        "Обновление искать на github.com/egor2998067/Perception-of-music",
+        "",
+        "PreHack.ru"};
+            resources.ApplyResources(this.metroTextBox1, "metroTextBox1");
+            this.metroTextBox1.MaxLength = 32767;
+            this.metroTextBox1.Multiline = true;
+            this.metroTextBox1.Name = "metroTextBox1";
+            this.metroTextBox1.PasswordChar = '\0';
+            this.metroTextBox1.ReadOnly = true;
+            this.metroTextBox1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.metroTextBox1.SelectedText = "";
+            this.metroTextBox1.Text = global::VK.Properties.Settings.Default.def;
+            this.metroTextBox1.UseSelectable = true;
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            resources.ApplyResources(this.axWindowsMediaPlayer1, "axWindowsMediaPlayer1");
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            // 
+            // metroButton2
+            // 
+            this.metroButton2.FontSize = MetroFramework.MetroButtonSize.Medium;
+            this.metroButton2.FontWeight = MetroFramework.MetroButtonWeight.Light;
+            resources.ApplyResources(this.metroButton2, "metroButton2");
+            this.metroButton2.Name = "metroButton2";
+            this.metroButton2.Style = MetroFramework.MetroColorStyle.Purple;
+            this.metroButton2.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroButton2.UseSelectable = true;
+            this.metroButton2.Click += new System.EventHandler(this.metroButton2_Click);
             // 
             // Form1
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
-            this.BackColor = System.Drawing.SystemColors.HighlightText;
             resources.ApplyResources(this, "$this");
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.metroButton2);
+            this.Controls.Add(this.metroTabControl1);
             this.Controls.Add(this.axWindowsMediaPlayer1);
             this.Controls.Add(this.pictureBox1);
             this.Cursor = System.Windows.Forms.Cursors.Default;
             this.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Opacity = 0.9D;
             this.ShowIcon = false;
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
             this.Resize += new System.EventHandler(this.Form1_Resize);
-            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.metroTabControl1.ResumeLayout(false);
+            this.metroTabPage1.ResumeLayout(false);
+            this.metroTabPage1.PerformLayout();
+            this.metroTabPage2.ResumeLayout(false);
+            this.metroTabPage2.PerformLayout();
+            this.metroTabPage3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -118,10 +293,24 @@
         private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
         private System.Windows.Forms.NotifyIcon notifyIcon1;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.Button button3;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private MetroFramework.Controls.MetroLabel metroLabel3;
+        private MetroFramework.Controls.MetroTabControl metroTabControl1;
+        private MetroFramework.Controls.MetroTabPage metroTabPage1;
+        private MetroFramework.Controls.MetroTabPage metroTabPage2;
+        private MetroFramework.Controls.MetroTabPage metroTabPage3;
+        private MetroFramework.Controls.MetroButton LoadCastomButton1;
+        private MetroFramework.Controls.MetroTextBox IDTextBox;
+        private MetroFramework.Controls.MetroLabel metroLabel4;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Controls.MetroLabel metroLabel6;
+        private MetroFramework.Controls.MetroComboBox GroupComboBox1;
+        private MetroFramework.Controls.MetroLabel metroLabel5;
+        private MetroFramework.Controls.MetroTextBox CountTextBox;
+        private MetroFramework.Controls.MetroTextBox metroTextBox1;
+        private MetroFramework.Controls.MetroButton metroButton2;
     }
 }
 
--- Исходник/VK/Form1.cs
+++ Исходник/VK/Form1.cs
@@ -1,38 +1,59 @@
 ﻿using System;
 using System.Windows.Forms;
 using System.Xml;
+using MetroFramework.Forms;
+
 namespace VK
 {
-    public partial class Form1 : Form
+    public partial class Form1 : MetroForm
     {
-        public static string getapi;
-        public string usedurl;
+        public static string getapi; // Готовый запрос
+        public static string paramid; // Часть запроса (Айди)
+        public static string paramc; // Часть запроса (кол во постов)
+
+        public static string PMgetMusic = "https://api.vk.com/method/wall.get.xml?owner_id=-35193970&count="; //Пресет
+        public static string MFCgetMusic = "https://api.vk.com/method/wall.get.xml?owner_id=-74779558&count="; // Тоже
+
+        public string usedurl; //Хуй знает что, забыл 
         public Form1(string[] args)
         {
             InitializeComponent();
+
+            //Аргументы из вне
             if (args.Length != 0)
             {
-                string paramid = args[0];
-                string paramc = args[1];
-                getapi = "https://api.vk.com/method/wall.get.xml?owner_id=" + paramid + "&count=" + paramc;
+                paramid = args[0];
+                paramc = args[1];
             }
-            else { getapi = "https://api.vk.com/method/wall.get.xml?owner_id=-35193970&count=100"; }
+            else {
+                paramid = "-35193970";
+                paramc = "100";
+            }
+            getapi = "https://api.vk.com/method/wall.get.xml?owner_id=" + paramid + "&count=" + paramc; //создание запроса
+            if (paramid == "-35193970") { metroLabel3.Text = "Group: Perception of music";} 
+            if (paramid == "-74779558") { metroLabel3.Text = "Group: Music for coding";}
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
 
-            try
+        //Функциия загрузки музыки
+        //Отправка запроса, создание плейлста
+        //В GetMusic - передать запрос
+        public void LoadMusic(string GetMusic) {
+           try
             {
+                listBox1.Items.Clear();
+                WMPLib.IWMPPlaylist PlayList;
+                WMPLib.IWMPMedia Media;
+
                 var doc = new XmlDocument();
-                doc.Load(new XmlTextReader(getapi));
-
+                doc.Load(new XmlTextReader(GetMusic));
                 var audioTags = doc.SelectNodes("//audio");
+               
                 PlayList = axWindowsMediaPlayer1.playlistCollection.newPlaylist("vkPlayList");
-
 
                 foreach (XmlNode audioTag in audioTags)
                 {
+
                     string artist = "";
                     string title = "";
                     string url = "";
@@ -41,21 +62,21 @@ namespace VK
                     if (audioTag["url"] != null) url = audioTag["url"].InnerText;
                     Media = axWindowsMediaPlayer1.newMedia(url);
                     PlayList.appendItem(Media);
-                    listBox1.Items.Add(title + " - " + artist);
+                    listBox1.Items.Add(artist + " – " + title);
 
                 }
                 axWindowsMediaPlayer1.currentPlaylist = PlayList;
+                metroLabel2.Text = "Load " + listBox1.Items.Count.ToString() + " items.";
             }
-
-            catch {
-                MessageBox.Show("Ошибка загрузки");
-            }
+           catch{MessageBox.Show("Ошибка загрузки(LoadMusic ERROR)");}
         }
-        WMPLib.IWMPPlaylist PlayList;
-        WMPLib.IWMPMedia Media;
-           
 
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            LoadMusic(getapi);
+        }
 
+        //иконка в треии
         private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
         {
             this.Show();
@@ -72,22 +93,55 @@ namespace VK
             }
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        //Выбор песни кликом в плейлисте
+        private void listBox1_MouseClick(object sender, MouseEventArgs e)
         {
-            MessageBox.Show("Автор - vk.com/id208497682 \n Обвновления искать на github.com/egor2998067");
+            if (e.Button == MouseButtons.Left) {
+                axWindowsMediaPlayer1.Ctlcontrols.currentItem = axWindowsMediaPlayer1.currentPlaylist.get_Item(listBox1.SelectedIndex);
+                axWindowsMediaPlayer1.Ctlcontrols.play();
+            }
+        }
+        //Загрузка группы по настройкам (Кастомная)
+        private void LoadCastomButton1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Хуева туча проверок. Работает и норм
+                double num = 0.0;
+                if (IDTextBox.Text != "" && CountTextBox.Text != "" && double.TryParse(CountTextBox.Text, out num) && double.TryParse(IDTextBox.Text, out num))
+                {
+                    int counif = int.Parse(CountTextBox.Text);
+                    if (counif < 100) { LoadMusic("https://api.vk.com/method/wall.get.xml?owner_id=-" + IDTextBox.Text + "&count=" + CountTextBox.Text); }
+                    else { LoadMusic("https://api.vk.com/method/wall.get.xml?owner_id=-" + IDTextBox.Text + "&count=100"); }
+                }
+                metroLabel3.Text = "";
+                if (IDTextBox.Text == "35193970") { metroLabel3.Text = "Group: Perception of music"; }
+                if (IDTextBox.Text == "74779558") { metroLabel3.Text = "Group: Music for coding"; }
+            }
+            catch {MessageBox.Show("Ошибка загрузки (LoadCastomButton ERROR)");}
+        }
+        // Пресеты групп
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            try { 
+            switch(GroupComboBox1.SelectedIndex) {
+                case 0:
+                    LoadMusic(PMgetMusic + "100");
+                    metroLabel3.Text = "Group: Perception of music";
+                    break;
+                case 1:
+                    LoadMusic(MFCgetMusic + "100");
+                    metroLabel3.Text = "Group: Music for coding";
+                    break;
+                default: break;
+                }
+            }
+            catch { MessageBox.Show("Ошибка загрузки (GroupComboBox1 ERROR)"); }
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void metroButton2_Click(object sender, EventArgs e)
         {
-            listBox1.Visible = true;
-            button3.Visible = true;
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            button3.Visible = false;
-            listBox1.Visible = false;
+            if (this.Width == 664) { this.Width = 344; metroButton2.Text = "Maximize"; } else { this.Width = 664; metroButton2.Text = "Minimize"; }
         }
     }
-    
 }
